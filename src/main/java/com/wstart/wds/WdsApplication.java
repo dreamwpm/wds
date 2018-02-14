8bdeaf93296d248febc6be4719b6438afa895ab6
john.wu.wu <john.wu.wu@ericsson.com>
2018-02-14 10:38:21 +0800

    first commit

---
+++ src/main/java/com/wstart/wds/WdsApplication.java
@@ -0,0 +1,12 @@
+package com.wstart.wds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdsApplication.class, args);
+	}
+}
